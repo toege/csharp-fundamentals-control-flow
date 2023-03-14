d8e50d502ebf7c8ed32192c9d2725fe729d8c50c
uerbzr <nigel.sibbert@gmail.com>
2023-03-14 14:57:45 +0000

    #207 added extension

--- csharp-fundamentals-control-flow.Main/Extension.cs
+++ csharp-fundamentals-control-flow.Main/Extension.cs
@@ -6,7 +6,39 @@ using System.Threading.Tasks;
 
 namespace csharp_fundamentals_control_flow.Main
 {
-    internal class Extension
+    public class Extension
     {
+
+
+        /* 
+         Extension: 1.  We're going to improve our cake baking capabilities!
+        //TODO: Create a public method named timerStatus that accepts one parameter:
+        - the number of minutes left on the timer
+        The method must return "The cake is ready!" if the remaining minutes is 0,
+        "The cake is still baking!" if there are any remaining minutes left,
+        and "The timer finished ages ago!" if the remaining minutes is a negative number
+        */
+
+
+
+
+        //TODO: Extension: 2. Create a method named estimatePrepTime that accepts two parameters:
+        /*  - an array of ingredients, e.g. ["sugar", "milk", "flour", "eggs"]
+            - the prep time per ingredient in minutes
+            The method must return the total prep time required based on the number of ingredients
+            provided and the prep time per ingredient.
+            If a prep time of 0 is provided, the method should assume each ingredient takes 2 minutes to prepare.
+         */
+
+
+
+
+        //TODO: 3.  Create a method named calculateGramsOfSugar that accepts two parameters:
+        /*  - an array of ingredients that will always contain 3 ingredients
+            - the number of layers the cake has
+            The cake will need 100g of sugar per layer, if that ingredient is present in the provided list of ingredients.
+            The method should return the number of grams of sugar needed to make the cake.
+         */
+
     }
 }
---
+++ csharp-fundamentals-control-flow.Test/ExtensionTest.cs
@@ -0,0 +1,50 @@
+﻿using csharp_fundamentals_control_flow.Main;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_fundamentals_control_flow.Test
+{
+    public class ExtensionTest
+    {
+        Extension extension;
+
+        public ExtensionTest()
+        {
+            this.extension = new Extension();
+        }
+
+        [Test]
+        public void one()
+        {
+            Assert.AreEqual("The cake is ready!", this.extension.timerStatus(0));
+            Assert.AreEqual("The cake is still baking!", this.extension.timerStatus(1));
+            Assert.AreEqual("The cake is still baking!", this.extension.timerStatus(23));
+            Assert.AreEqual("The timer finished ages ago!", this.extension.timerStatus(-1));
+            Assert.AreEqual("The timer finished ages ago!", this.extension.timerStatus(-25));
+        }
+
+        [Test]
+        public void two()
+        {
+            Assert.AreEqual(5, this.extension.estimatePrepTime(new String[] { "milk", "sugar", "eggs", "flour", "chocolate" }, 1));
+            Assert.AreEqual(10, this.extension.estimatePrepTime(new String[] { "milk", "sugar", "eggs", "flour", "chocolate" }, 2));
+            Assert.AreEqual(10, this.extension.estimatePrepTime(new String[] { "milk", "sugar", "eggs", "flour", "chocolate" }, 0));
+            Assert.AreEqual(20, this.extension.estimatePrepTime(new String[] { "milk", "sugar" }, 10));
+        } 
+        
+        [Test] 
+        public void three()
+        {
+            Assert.AreEqual(0, this.extension.calculateGramsOfSugar(new String[] { "milk", "flour", "eggs" }, 3));
+            Assert.AreEqual(100, this.extension.calculateGramsOfSugar(new String[] { "milk", "flour", "sugar" }, 1));
+            Assert.AreEqual(200, this.extension.calculateGramsOfSugar(new String[] { "milk", "sugar", "eggs" }, 2));
+            Assert.AreEqual(300, this.extension.calculateGramsOfSugar(new String[] { "sugar", "flour", "eggs" }, 3));
+            Assert.AreEqual(400, this.extension.calculateGramsOfSugar(new String[] { "sugar", "milk", "eggs" }, 4));
+        }
+    }
+
+}
